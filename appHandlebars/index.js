a4d30a04570d6d9d3e9fded8e87332284964ff62
Gustavo <gustavosantos4991@gmail.com>
2023-05-15 22:45:24 -0300

    Aula1505 - Métodos Handlebars

--- appHandlebars/index.js
+++ appHandlebars/index.js
@@ -6,6 +6,21 @@ const app = express()// construtor
 
 app.engine('handlebars', exphbs())
 app.set('view engine', 'handlebars')
+const approved = false
+
+app.post('/verifyUser', (req, res) => {
+    const form = {
+        name:req.body.name,
+        idade:req.body.age
+    }
+    if(form.name == 'Gisley' && form.age == 52){
+        approved = true
+    }
+    else{
+        approved = false
+    }
+    res.send(approved)
+})
 
 app.get('/', function(req, res){
     const user = {
@@ -13,7 +28,10 @@ app.get('/', function(req, res){
         surname: "Santos",
         age: "28"
     }
-    res.render('home', {user:user})
+    res.render('home', {user:user, auth:approved, approved})
+})
+app.get('/dashboards', function(req, res){
+    res.render('dashboards')
 })
 
 app.listen(3000)
