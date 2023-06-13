ebb153b3e81f925f1f0266524b513b36541881d8
Gustavo <gustavosantos4991@gmail.com>
2023-06-12 22:39:28 -0300

    Aula1206 - Sessão e Cookie

--- appThoughts/index.js
+++ appThoughts/index.js
@@ -4,6 +4,7 @@ const exphbs = require('express-handlebars')
 const session = require('express-session')
 const FileStore = require('session-file-store')(session)
 const flash = require('express-flash')
+const authRoutes = require('./routes/authRoutes')
 const app = express()
 //#endregion
 
@@ -12,11 +13,47 @@ app.engine('handlebars', exphbs())
 app.set('view engine', 'handlebars')
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
+app.use(express.static('public'))
 //#endregion
 
 const conn = require('./db/conn')
 const Thought = require('./models/Thought')
 const User = require('./models/User')
+
+//region Rotas de controle
+app.use('/', authRoutes)
+//endregion
+
+//region Controle de Sessão
+app.use(
+    session({
+        name: 'session',
+        secret: 'nosso_secret',
+        saveUninitialized: false,
+        store: new FileStore({
+            logFn: function(){},
+            path: require('path').join(require('os').tmpdir(), 'sessions')
+        }),
+        cookie: {
+            secure: false,
+            maxAge: 3600000,
+            expires: new Date(Date.now() + 3600000),
+            httpOnly: true
+        }
+    })
+)
+
+app.use(flash())
+app.use((req, res, next) => {
+    console.log(req.session.userid)
+
+    if(req.session.userid){
+        res.locals.session = req.session
+    }
+
+    next()
+})
+//endregion
 
 conn
 .sync()
